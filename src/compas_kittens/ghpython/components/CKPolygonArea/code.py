c93fa311afde49fc2ba3261bc4fceec7985fc171
Gonzalo CasasMatthias <kohler@arch.ethz.ch>
2023-05-08 14:44:13 +0200

    formatting

--- src/compas_kittens/ghpython/components/CKPolygonArea/code.py
+++ src/compas_kittens/ghpython/components/CKPolygonArea/code.py
@@ -1,6 +1,7 @@
 from compas_kittens.algorithms import polygon_area
 from ghpythonlib.componentbase import executingcomponent as component
 
+
 class CKPolygonArea(component):
     def RunScript(self, polygon):
         return polygon_area(polygon)
